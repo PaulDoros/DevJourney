8058b32040d30e7562c81a979491f5d84cada879
Paul Doros <dorospaul26@gmail.com>
2025-01-20 12:19:23 +0200

    de

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,27 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { vercelPreset } from '@vercel/remix/vite';
 
+declare module '@remix-run/node' {
+  interface Future {
+    v3_fetcherPersist: true;
+    v3_relativeSplatPath: true;
+    v3_throwAbortReason: true;
+    v3_singleFetch: true;
+    v3_lazyRouteDiscovery: true;
+  }
+}
+
 export default defineConfig({
   plugins: [
     remix({
       presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
     }),
     tsconfigPaths(),
   ],
