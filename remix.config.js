9737a45e1ad4f9938e92f5953785748f1128b3ae
Paul Doros <dorospaul26@gmail.com>
2025-01-20 13:15:18 +0200

    remix route

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  serverModuleFormat: 'esm',
+  serverModuleFormat: 'cjs',
+  serverBuildPath: 'build/server/index.js',
+  assetsBuildDirectory: 'build/client',
+  publicPath: '/build/',
   dev: {
     port: process.env.PORT || 3000,
     hmr: {
@@ -9,10 +12,10 @@ module.exports = {
     },
   },
   future: {
-    v3_fetcherPersist: true,
-    v3_relativeSplatPath: true,
-    v3_throwAbortReason: true,
-    v3_singleFetch: true,
-    v3_lazyRouteDiscovery: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
   },
 };
