e2559693942f467bba70ce6076c301802a188724
Paul Doros <dorospaul26@gmail.com>
2025-01-21 11:35:16 +0200

    added remix utils

--- app/components/AnimatedAvatar.tsx
+++ app/components/AnimatedAvatar.tsx
@@ -1,4 +1,5 @@
 import { Player } from '@lottiefiles/react-lottie-player';
+import { useHydrated } from 'remix-utils/use-hydrated';
 
 interface AnimatedAvatarProps {
   url: string;
@@ -11,6 +12,8 @@ export const AnimatedAvatar = ({
   size = 'md',
   className,
 }: AnimatedAvatarProps) => {
+  const isHydrated = useHydrated();
+
   const sizeClasses = {
     sm: 'h-8 w-8',
     md: 'h-12 w-12',
@@ -21,21 +24,23 @@ export const AnimatedAvatar = ({
     <div
       className={`${sizeClasses[size]} ${
         className ?? ''
-      } relative overflow-hidden rounded-full`}
+      } relative overflow-hidden rounded-full bg-gray-100`}
     >
-      <Player
-        src={url}
-        autoplay
-        loop
-        style={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-        }}
-      />
+      {isHydrated && (
+        <Player
+          src={url}
+          autoplay
+          loop
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        />
+      )}
     </div>
   );
 };
